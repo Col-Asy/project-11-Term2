10e122b59021e353f1b494fb590056d39d777e4e
Mohak <mohakmalhotra008@gmail.com>
2022-02-06 12:46:09 +0530

    modified sleep with time

--- sleep.py
+++ sleep.py
@@ -1,11 +1,24 @@
+"""Importing tkinter and time module"""
 from tkinter import *
+import time 
 
+
+
+
+"""defining sleep screen"""
 root = Tk()
 root.title('Sleep Screen')
 root.iconbitmap('image/prompt_icon.ico')
 
-root.config(bg="black")
 
+"""setting the background for screen 
+as black and zoomed out for full mode"""
+root.config(bg="black")
 root.state('zoomed')
+
+"""after a certain duration 
+the screen will withdraw"""
+time.sleep(5)
+root.withdraw()
 
 root.mainloop()
