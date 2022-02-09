971fefed46c072e0e31a8f61fe5ae2eefe4c09af
Mohak <mohakmalhotra008@gmail.com>
2022-02-09 23:21:24 +0530

    changes

--- welcome.py
+++ welcome.py
@@ -1,54 +1,56 @@
 from tkinter import *
 
 
-def btn_clicked():
-    print("Button Clicked")
 
 
-window = Tk()
+def welcome_screen():
+    window = Tk()
 
-window.state('zoomed')
-window.configure(bg = "#ffffff")
-canvas = Canvas(
-    window,
-    bg = "#ffffff",
-    height = 600,
-    width = 1000,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge")
-canvas.place(x = 0, y = 0)
+    window.state('zoomed')
+    window.configure(bg = "#ffffff")
+    canvas = Canvas(
+        window,
+        bg = "#ffffff",
+        height = 600,
+        width = 1000,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge")
+    canvas.place(x = 0, y = 0)
 
-background_img = PhotoImage(file = "resources/background.png")
-background = canvas.create_image(
-    750.0, 320.0,
-    image=background_img)
+    def close_window():
+        
 
-img0 = PhotoImage(file = "resources/img0.png")
-b0 = Button(
-    image = img0,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    background_img = PhotoImage(file = "resources/background.png")
+    background = canvas.create_image(
+        750.0, 320.0,
+        image=background_img)
 
-b0.place(
-    x = 875, y = 425,
-    width = 463,
-    height = 101)
+    img0 = PhotoImage(file = "resources/img0.png")
+    b0 = Button(
+        image = img0,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked,
+        relief = "flat")
 
-img1 = PhotoImage(file = "resources/img1.png")
-b1 = Button(
-    image = img1,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
+    b0.place(
+        x = 875, y = 425,
+        width = 463,
+        height = 101)
 
-b1.place(
-    x = 170, y = 425,
-    width = 463,
-    height = 101)
+    img1 = PhotoImage(file = "resources/img1.png")
+    b1 = Button(
+        image = img1,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = close_window,
+        relief = "flat")
 
-window.resizable(False, False)
-window.mainloop()
+    b1.place(
+        x = 170, y = 425,
+        width = 463,
+        height = 101)
+
+    window.resizable(False, False)
+    window.mainloop()
