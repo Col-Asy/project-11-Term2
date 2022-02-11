2290179d184ac3541d6a973b920fa89cfbde3b5a
Mohak <mohakmalhotra008@gmail.com>
2022-02-11 22:06:58 +0530

    compeleted

--- welcome.py
+++ welcome.py
@@ -11,8 +11,8 @@ def welcome_screen(config):
     canvas = Canvas(
         window,
         bg = "#ffffff",
-        height = 600,
-        width = 1000,
+        height = 2040,
+        width = 2040,
         bd = 0,
         highlightthickness = 0,
         relief = "ridge")
@@ -29,13 +29,24 @@ def welcome_screen(config):
 
     background_img = PhotoImage(file = "image/welcome.png")
     background = canvas.create_image(
-        750.0, 320.0,
+        750.0, 250.0,
         image=background_img)
     active = config["active"]
     sleep = config["sleep"]
     canvas.create_text(
-    600, 500,
-    text = (f"The current settings are screen time: {active} and break time: {sleep}"),
+    750, 350,
+    text = ("The current settings are"),
+    fill = "#000000",
+    font = ("Montserrat-Medium", int(48.0) ) )
+
+    canvas.create_text(
+    750, 450,
+    text = (f"screen time: {active} minutes"),
+    fill = "#000000",
+    font = ("Montserrat-Medium", int(48.0) ) )
+    canvas.create_text(
+    750, 550,
+    text = (f"break time: {sleep} seconds"),
     fill = "#000000",
     font = ("Montserrat-Medium", int(48.0) ) )
 
@@ -49,7 +60,7 @@ def welcome_screen(config):
         relief = "flat")
 
     b0.place(
-        x = 875, y = 685,
+        x = 875, y = 600,
         width = 463,
         height = 101)
 
@@ -62,7 +73,7 @@ def welcome_screen(config):
         relief = "flat")
 
     b1.place(
-        x = 170, y = 685,
+        x = 170, y = 600,
         width = 463,
         height = 101)
 
